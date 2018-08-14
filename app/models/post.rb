9409dd5ecbb2a67793b240de02e393e2626822b0
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-14 09:51:43 +0100

    Jo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  # validates_inclusion_of :title, {:in => ["Won't Believe", "Secret", "Top[number]", "Guess"] }
+  validate :includes_clickbait?
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates_inclusion_of :category, :in => %w(Fiction Non-Fiction)
+
+def includes_clickbait?
+  array = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+  if title != nil && array.find {|clickbait| title.include?(clickbait)} == nil
+    errors.add(:title, "needs to be more clickbaity")
+  end
+end
+
 end
